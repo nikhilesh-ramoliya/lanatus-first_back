ffdae3e2b18854f065dc47d7f810b3987e13f914
niskPatel1 <ramoliyanikhil252525@gmail.com>
2022-10-06 09:02:23 +0530

    mongodb connection

--- Connections.js
+++ Connections.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
-const connection = mongoose.connect('mongodb://localhost:27017/lanatus').then(()=>{
+const url = "mongodb+srv://first:first@cluster0.zpldju3.mongodb.net/?retryWrites=true&w=majority"
+
+const connection = mongoose.connect(url).then(()=>{
 console.log('data base is connected');
 })
 
